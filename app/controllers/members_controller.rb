369f78e96acf96b4c42e199ed6926fbeacd72948
ryoryo1121 <ry01121xx@gmail.com>
2018-12-26 00:08:00 +0900

    add members index
    index アクション　ビューの追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,23 @@
 class MembersController < ApplicationController
+  def index
+    @members = Member.order("number")
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
