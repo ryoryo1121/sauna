2bb7f0cde39ee5e3c7f23660b330c72926f5ea7e
ryoryo1121 <ry01121xx@gmail.com>
2018-12-30 17:20:39 +0900

    add like function
    いいね機能の追加

--- db/seeds/development/entries.rb
+++ db/seeds/development/entries.rb
@@ -1,8 +1,8 @@
 body =
-  "今晩は久しぶりに神宮で野球観戦。内野B席の上段に着席。\n" +
+  "今晩は久しぶりに神宮で野球観戦。内野B席の上段に着席。\n\n" +
   "先発はヤクルトがブキャナン、広島はジョンソン。" +
   "2回裏に中村選手のセーフティスクイズなどでヤクルトが3点を先取。" +
-  "そして、8回裏には代打・荒木選手がレフトスタンドへ2号満塁ホームラン。\n" +
+  "そして、8回裏には代打・荒木選手がレフトスタンドへ2号満塁ホームラン。\n\n" +
   "ブキャナン投手の今季初完封を見届けて、気分良く家路に着きました。"
 
 %w(Taro Jiro Hana).each do |name|
@@ -13,7 +13,9 @@ body =
       title: "野球観戦#{idx}",
       body: body,
       posted_at: 10.days.ago.advance(days: idx),
-      status: %w(draft member_only public)[idx % 3])
+      status: %w(draft member_only public)[idx % 3]
+    )
+
     if idx == 7 || idx == 8
       %w(John Mike Sophy).each do |name2|
         voter = Member.find_by(name: name2)
