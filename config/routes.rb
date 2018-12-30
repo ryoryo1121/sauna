32066d06abee57546134e2281a556a1f55242aa3
ryoryo1121 <ry01121xx@gmail.com>
2018-12-30 18:30:27 +0900

    admin page
    管理者ようページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
 
-  resources :members do
+  resources :members , only: [:index, :show] do
     get "search", on: :collection
     resources :entries, only: [:index]
   end
@@ -14,12 +14,20 @@ Rails.application.routes.draw do
   resource :account, only: [:show, :edit, :update]
   resource :password, only: [:show, :edit, :update]
 
-  resources :articles
+  resources :articles, only: [:index, :show]
   resources :entries do
     patch "like", "unlike", on: :member
     get "voted", on: :collection
     resources :images, controller: "entry_images" do
       patch :move_igher, :move_lower, on: :member
     end
+  end
+
+  namespace :admin do
+    root "top#index"
+    resources :members do
+      get "search", on: :collection
+    end
+    resources :articles
   end
 end
