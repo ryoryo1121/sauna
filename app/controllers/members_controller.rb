48099613062c0b4b1d5b75233e98bfad57cfea4a
ryoryo1121 <ry01121xx@gmail.com>
2018-12-30 11:18:11 +0900

    add error view
    エラー画面の追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -18,7 +18,7 @@ class MembersController < ApplicationController
   end
 
   def create
-    @member = Member.new(params[:member])
+    @member = Member.new(member_params)
     if @member.save
       redirect_to @member, notice: "会員を登録しました"
     else
@@ -28,7 +28,7 @@ class MembersController < ApplicationController
 
   def update
     @member = Member.find(params[:id])
-    @member.assign_attributes(params[:member])
+    @member.assign_attributes(member_params)
     if @member.save
       redirect_to @member, notice: "会員情報を更新しました"
     else
@@ -46,5 +46,22 @@ class MembersController < ApplicationController
     @members = Member.search(params[:q])
       .page(params[:page]).per(15)
     render "index"
+  end
+
+  # ストロングパラメーター
+  private def mamber_params
+    attrs = [
+      :number,
+      :name,
+      :full_name,
+      :sex,
+      :birthday,
+      :email,
+      :administrator
+    ]
+
+    attrs >> :password if params[:action] == "create"
+
+    params.require(:member).permit(attrs)
   end
 end
